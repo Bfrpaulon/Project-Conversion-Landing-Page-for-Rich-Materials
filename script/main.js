7c4b5a0be618e31a857607a75cfd4978f17cb193
Bruno Paulon <122818137+Bfrpaulon@users.noreply.github.com>
2023-02-02 15:48:44 +0000

    update index.html, main.js and main.css

--- script/main.js
+++ script/main.js
@@ -1,4 +1,42 @@
-document.getElementById("download-ebook-button").addEventListener("click", function() {
-    window.location.href = "downloads/ebook.pdf";
-  });
-  
+ // Enable button 
+
+ const form = document.querySelector("#form-body");
+ const nameInput = document.querySelector("#name");
+ const emailInput = document.querySelector("#email");
+ const downloadButton = document.querySelector("#download-button");
+
+ form.addEventListener("submit", (event) => {
+   event.preventDefault();
+
+   if (nameInput.value === "" || emailInput.value === "") {
+     alert("Please fill in both name and email fields");
+     return;
+   }
+
+   // Trigger the actual download of the ebook
+   const ebookURL = "/ebook.pdf";
+   const ebookName = "ebook.pdf";
+   const a = document.createElement("a");
+   a.style.display = "none";
+   a.href = ebookURL;
+   a.download = ebookName;
+   document.body.appendChild(a);
+   a.click();
+   document.body.removeChild(a);
+ });
+
+ nameInput.addEventListener("input", () => {
+   if (nameInput.value !== "" && emailInput.value !== "") {
+     downloadButton.removeAttribute("disabled");
+   } else {
+     downloadButton.setAttribute("disabled", true);
+   }
+ });
+
+ emailInput.addEventListener("input", () => {
+   if (nameInput.value !== "" && emailInput.value !== "") {
+     downloadButton.removeAttribute("disabled");
+   } else {
+     downloadButton.setAttribute("disabled", true);
+   }
+ });
